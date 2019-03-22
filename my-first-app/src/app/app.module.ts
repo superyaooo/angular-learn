4a58d840c1dfeb3072ce4b7702926cba134ce2b9
superyaooo <superyaooo@gmail.com>
2019-03-22 15:59:10 -0500

    add router outlet

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,12 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    // no need to place any providers due to the 'providedIn' flag
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
