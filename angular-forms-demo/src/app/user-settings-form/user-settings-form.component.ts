98371ffce1baf3ebce1a42c74d56e8ae1dbd35a2
superyaooo <superyaooo@gmail.com>
2019-06-23 21:26:44 -0500

    form validation on angular form

--- angular-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ angular-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -9,7 +10,7 @@ import { UserSettings } from '../data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   originalUserSettings: UserSettings = {
-    name: 'Milton',
+    name: null,
     emailOffers: true,
     interfaceStyle: 'dark',
     subscriptionType: 'Annual',
@@ -22,4 +23,11 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
 }
