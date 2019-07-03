a593c0d363ac8dde190621135197fe3aed70a88f
superyaooo <superyaooo@gmail.com>
2019-07-02 20:31:33 -0500

    observables & async pipe

--- angular-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ angular-forms-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -18,13 +20,35 @@ export class UserSettingsFormComponent implements OnInit {
   };
 
   userSettings: UserSettings = {...this.originalUserSettings};  // the spread operator -> ...
-  constructor() { }
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes: Observable<string[]>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
   }
 
   onSubmit(form: NgForm) {
     console.log('in onSubmit: ', form.valid);
+
+    if(form.valid) {
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log('success: ', result),
+        error => this.onHttpError(error)
+      );
+    }
+    else {
+      this.postError = true;
+      this.postErrorMessage = "please fix this shit";
+    }
   }
 
   onBlur(field: NgModel) {
