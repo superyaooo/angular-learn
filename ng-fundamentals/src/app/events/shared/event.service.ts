6b7972f46814518597f72eb44427df7b2a80365f
superyaooo <superyaooo@gmail.com>
2019-12-20 13:48:30 -0600

    more templating

--- ng-fundamentals/src/app/events/shared/event.service.ts
+++ ng-fundamentals/src/app/events/shared/event.service.ts
@@ -4,6 +4,11 @@ import { IEvent } from './event.model';
 
 @Injectable()
 export class EventService {
+    saveEvent(event) {
+        event.id = 999;
+        event.session = [];
+        EVENTS.push(event);
+    }
     getEvents():Observable<IEvent[]> {
         let subject = new Subject<IEvent[]>();
         setTimeout(() => 
